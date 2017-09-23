ea54f509ab2bec16c5531381f64c1cbc4a071c4d
rrw23 <r.r.wareing@gmail.com>
2017-09-24 09:55:27 +1300

    UpdatedTools
    General development updates

--- noiseTools_20170922.py
+++ noiseTools_20170922.py
@@ -57,53 +57,114 @@ def speedOfSound(temp):
     T = 273 + temp
     return math.sqrt(y*R*T/M)
 
-def ThirdOctaveWeightingCurves(wieghtingType,startFreq,stopFreq):
-    aa
-    if wieghtingType == "A":
-        weighting = [['frequencies'],
-                     ['corrections']]
+def ThirdOctaveWeightingCurves(weightingType,startFreq,stopFreq):
+    if weightingType == "A":
+        rawWeighting = [[10,12.5,16,20,
+                      35,31.5,40,50,
+                      63,80,100,125,
+                      160,200,250,315,
+                      400,500,630,800,
+                      1000,1250,1600,2000,
+                      2500,3150,4000,5000,
+                      6300,8000,10000,12500,
+                      16000,20000],
+                     [-70.4,-63.4,-56.7,-50.5,
+                      -44.7,-39.4,-34.6,-30.2,
+                      -26.2,-22.5,-19.1,-16.1,
+                      -13.4,-10.9,-8.6,-6.6,
+                      -4.8,-3.2,-1.9,-0.8,
+                      0.0,0.6,1.0,1.2,
+                      1.3,1.2,1.0,0.5,
+                      -0.1,-1.1,-2.5,-4.3,
+                      -6.6,-9.3]]
     elif weightingType == "C":
-        weighting = [['frequencies'],
-                     ['corrections']]
+        rawWeighting = [[10,12.5,16,20,
+                      35,31.5,40,50,
+                      63,80,100,125,
+                      160,200,250,315,
+                      400,500,630,800,
+                      1000,1250,1600,2000,
+                      2500,3150,4000,5000,
+                      6300,8000,10000,12500,
+                      16000,20000],
+                     [-14.3,-11.2,-8.5,-6.2,
+                      -4.4,-3.0,-2.0,-1.3,
+                      -0.8,-0.5,-0.3,-0.2,
+                      -0.1,0.0,0.0,0.0,
+                      0.0,0.0,0.0,0.0,
+                      0.0,0.0,-0.1,-0.2,-0.3,
+                      -1.3,-2.0,-3.0,-4.4,
+                      -6.2,-8.5,-11.2]]
+    weights = []
+    frequencies = []
+    for i in range(len(rawWeighting[0])):
+        if rawWeighting[0][i] >= startFreq and rawWeighting[0][i] <= stopFreq:
+            weights.append(rawWeighting[1][i])
+            frequencies.append(rawWeighting[0][i])
+    weighting = [frequencies,weights]
+    return weighting
+
+def OctaveWeightingCurves(weightingType,startFreq,stopFreq):
+    if weightingType == "A":
+        rawWeighting = [[31.5,63,125,
+                       250,500,1000,2000,
+                       4000,8000],
+                     [-39.4,-26.2,-16.1,-8.6,
+                      -3.2,0,1.2,1,-1.1
+                      ]]
+    elif weightingType == "C":
+        rawWeighting = [[31.5,63,125,
+                       250,500,1000,2000,
+                       4000,8000],
+                     [-3,-0.8,-0.2,0,
+                      0,0,-0.2,-0.8,-3]]
+    weights = []
+    frequencies = []
+    for i in range(len(rawWeighting[0])):
+        if rawWeighting[0][i] >= startFreq and rawWeighting[0][i] <= stopFreq:
+            weights.append(rawWeighting[1][i])
+            frequencies.append(rawWeighting[0][i])
+    weighting = [frequencies,weights]
     return weighting
     
 def dBA(levels,freq,bandType):
     weightedLevel = []
     if bandType == "1/1":
-        weightingCurve = ThirdOctaveWeightingCurves("A",freq[0],freq[-1])
-    elif bandType == "1/3":
         weightingCurve = OctaveWeightingCurves("A",freq[0],freq[-1])
-    for i in range(levels):
-        weightedLevel.append(levels[i] + wieghtingCurve[1][i])
+    elif bandType == "1/3":
+        weightingCurve = ThirdOctaveWeightingCurves("A",freq[0],freq[-1])
+    for i in range(len(levels)):
+        weightedLevel.append(levels[i] + weightingCurve[1][i])
     return dBadd(weightedLevel)
     
 def dBC(levels,freq,bandType):
     weightedLevel = []
     if bandType == "1/1":
-        weightingCurve = ThirdOctaveWeightingCurves("C",freq[0],freq[-1])
-    elif bandType == "1/3":
         weightingCurve = OctaveWeightingCurves("C",freq[0],freq[-1])
-    for i in range(levels):
-        weightedLevel.append(levels[i] + wieghtingCurve[1][i])
+    elif bandType == "1/3":
+        weightingCurve = ThirdOctaveWeightingCurves("C",freq[0],freq[-1])
+    for i in range(len(levels)):
+        weightedLevel.append(levels[i] + weightingCurve[1][i])
     return dBadd(weightedLevel)
     
 def dBadd(levels):
-    pressureAbs
+    pressureAbs = 0
     for level in levels:
         pressureAbs = pressureAbs + 10**(level/10)
     return 10*math.log10(pressureAbs)
     
 def dBavg(levels):
-    pressureAbs
+    pressureAbs = 0
     for level in levels:
         pressureAbs = pressureAbs + 10**(level/10)
     return 10*math.log10(pressureAbs/len(levels))
     
-def simpleCRTN(annualAvgTraffic,percentageHeavyVehicles,speed,gradient,surfaceCorrection,receiverDist,angleOfView)::
+def simpleCRTN(annualAvgTraffic,percentageHeavyVehicles,speed,gradient,
+               surfaceCorrection,receiverDist,angleOfView):
+    ''' Nothing in her yet...'''
     
 def simpleISO9140():
-    
-
+    '''Nothin in here yet'''
 
 print("Check propigation loss")
 print(BS5228_PropigationLoss(100,10))
@@ -115,3 +176,25 @@ print("Check octaves")
 print(BuildOctave(50,500))
 print("Check speed of sound")
 print(speedOfSound(20))
+print('Check A-weighting')
+print(ThirdOctaveWeightingCurves("A",50,500))
+print(OctaveWeightingCurves("A",50,500))
+print('Check C-weighting')
+print(ThirdOctaveWeightingCurves("C",50,500))
+print(OctaveWeightingCurves("C",50,500))
+print('Check dBA - 1/3')
+print(print(dBA([90,90,90],[50,63,80],"1/3")))
+print('Check dBA - 1/1')
+print(print(dBA([90,90,90],[63,125,250],"1/1")))
+print('Check dBC - 1/3')
+print(print(dBC([90,90,90],[50,63,80],"1/3")))
+print('Check dBC - 1/1')
+print(print(dBC([90,90,90],[63,125,250],"1/1")))
+print('Check dBadd')
+print(dBadd([90,90]))
+print('Check dBavg')
+print(dBavg([90,90,90]))
+print('Check simple CRTN')
+print('-------')
+print('Check ISO9140')
+print('-------')
